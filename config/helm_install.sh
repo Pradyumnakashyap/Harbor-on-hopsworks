5e9934e8f6d9a86dc8c868ab69146b77a3b99f3e
Pradyumna kashyap <pradyumnakas@gmail.com>
2020-06-05 12:27:29 +0200

    Update helm_install.sh

--- config/helm_install.sh
+++ config/helm_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install helm and move to the correct path
+# Install helm and move to the correct path - ***Root user privileges are required***
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
