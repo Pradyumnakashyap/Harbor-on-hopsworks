de2f554814bcd4d548e8cbb9a567a1d51525b094
Pradyumna kashyap <pradyumnakas@gmail.com>
2020-07-13 16:48:30 +0200

    Update perf-metrics.py

--- test/perf-metrics.py
+++ test/perf-metrics.py
@@ -13,9 +13,9 @@ repo_address = "172.20.9.16:30003"                # Harbor host
 
 repo_ref = "/test-1"
 repo_url = repo_address + repo_ref
-container_name = "nginx"
-container_tag = "latest"
-work_dir = "containers"
+image_name = "nginx"
+image_tag = "latest"
+work_dir = "images"
 build_results_file = "build_results.csv"
 push_results_file = "push_results.csv"
 pull_results_file = "pull_results.csv"
@@ -30,9 +30,9 @@ for results_file in results_files:
 work_queue = Queue()
 
 
-def build_container(iteration):
+def build_(iteration):
     start_time = time()
-    build_command = Popen(['docker', 'build', '--no-cache=true', '-t', repo_url + '/' + container_name + '-' + str(iteration) + ':' + container_tag, '--file=' + work_dir + '/Dockerfile', work_dir])
+    build_command = Popen(['docker', 'build', '--no-cache=true', '-t', repo_url + '/' + image_name + '-' + str(iteration) + ':' + image_tag, '--file=' + work_dir + '/Dockerfile', work_dir])
     build_command.wait()
     end_time = time()
     action_time = end_time - start_time
@@ -42,9 +42,9 @@ def build_container(iteration):
     outfile.close()
 
 
-def push_container(iteration):
+def push_image(iteration):
     start_time = time()
-    build_command = Popen(['docker', 'push', repo_url + '/' + container_name + '-' + str(iteration)])
+    build_command = Popen(['docker', 'push', repo_url + '/' + image_name + '-' + str(iteration)])
     build_command.wait()
     end_time = time()
     action_time = end_time - start_time
@@ -56,7 +56,7 @@ def push_container(iteration):
 
 def delete_local_images(iteration):
     start_time = time()
-    delete_local_images_command = Popen(['docker', 'rmi', repo_url + '/' + container_name + '-' + str(iteration)])
+    delete_local_images_command = Popen(['docker', 'rmi', repo_url + '/' + image_name + '-' + str(iteration)])
     delete_local_images_command.wait()
     end_time = time()
     action_time = end_time - start_time
@@ -66,9 +66,9 @@ def delete_local_images(iteration):
     outfile.close()
 
 
-def pull_container(iteration):
+def pull_image(iteration):
     start_time = time()
-    build_command = Popen(['docker', 'pull', repo_url + '/' + container_name + '-' + str(iteration)])
+    build_command = Popen(['docker', 'pull', repo_url + '/' + image_name + '-' + str(iteration)])
     build_command.wait()
     end_time = time()
     action_time = end_time - start_time
@@ -81,7 +81,7 @@ def pull_container(iteration):
 def repeat():
     while work_queue.empty() is False:
         iteration = work_queue.get_nowait()
-        container_action(iteration)
+        image_action(iteration)
         work_queue.task_done()
 
 
@@ -89,8 +89,8 @@ def fill_queue(iterations):
     for iteration in range(1, (iterations + 1)):
         work_queue.put(iteration)
 
-container_actions = [build_container, push_container, delete_local_images, pull_container]
-for container_action in container_actions:
+image_actions = [build_image, push_image, delete_local_images, pull_image]
+for image_action in image_actions:
     fill_queue(iterations)
     for thread_num in range(1, (concurrency + 1)):
         if work_queue.empty() is True:
