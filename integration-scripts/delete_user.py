7cb2e7141102462bfc33e08adb21d220f07eeba2
root <root@hopsworks0.logicalclocks.com>
2020-07-23 16:27:12 +0000

    updated exception handling and end cases for delete user script

--- integration-scripts/delete_user.py
+++ integration-scripts/delete_user.py
@@ -1,32 +1,30 @@
-#!/usr/bin/python
-
- #Operations delete users  
+#!/usr/bin/python 
 
 try:
-    import docker
     import sys
-    import time
     import requests
     import json
 
 except ImportError:
     import pip
     pip.main(['install', 'docker'])
-    import docker
+    import sys
     import requests
     import json
 
 def get_user_id(user_name):
 
-        resp = requests.get(r'https://{}/api/v2.0/users?username={}'.format(harbor_host,user_name),verify='/etc/docker/certs.d/{}/ca.crt'.format(harbor_host),auth=(user,password))
-        if resp.status_code != 200:
-    # if something went wrong.
-                print('Error fetching name "{}"  /api/projects {}'.format(user_name,resp.status_code))
-
-        resp_dump = json.dumps(resp.text)
-        resp_str = json.loads(resp_dump)
-        return [resp_str[21:24].split(',', 1)]
-
+        try:
+		resp = requests.get(r'https://{}/api/v2.0/users?username={}'.format(harbor_host,user_name),verify='/etc/docker/certs.d/{}/ca.crt'.format(harbor_host),auth=(user,password))
+        	if resp.status_code != 200:
+                	print('Error fetching username "{}" from Harbor with status code {}'.format(user_name,resp.status_code))
+			sys.exit(1)
+        	resp_dump = json.dumps(resp.text)
+        	resp_str = json.loads(resp_dump)
+        	return [resp_str[21:24].split(',', 1)]
+	except IOError:
+		print('Please verify the name of the Harbor registry passed as argument or check if the certificate required to access the Harbor Registry is located in path: "/etc/docker/certs.d/<registryip>/"!')
+		sys.exit(1)
 def delete_user(user_name):
 
         try:
@@ -35,18 +33,23 @@ def delete_user(user_name):
         	if user_id > 2:
                 	resp = requests.delete(r'https://{}/api/v2.0/users/{}'.format(harbor_host,user_id),verify=r'/etc/docker/certs.d/{}/ca.crt'.format(harbor_host),auth=(user,password),headers={'Content-Type':'application/json'})
                 	if resp.status_code != 200:
-                        	print('Error deleting user with status code /api/projects {}'.format(resp.status_code))
-                        	return
-                print('User successfully deleted')
-      
-	except ValueError as e:
+                        	print('Error deleting user with status code'.format(resp.status_code))
+                        	sys.exit(1)
+                #print('User successfully deleted')
+      	except ValueError as e:
       		print (r'User with the name {} does not exist!'.format(user_name))
-
+		sys.exit(1)
+	except:
+		print('User deletion unsuccessfull!')
+		sys.exit(1)
 
 if __name__ == '__main__':
 
         #Inputs to be obtained from Hopsworks
-        harbor_host= str(sys.argv[1])
+        if len(sys.argv)<5:
+                print("Fatal: Please include the required arguments for the script!")
+                sys.exit(1)
+	harbor_host= str(sys.argv[1])
 	user = str(sys.argv[2])
         password= str(sys.argv[3])
         user_name=str(sys.argv[4])
