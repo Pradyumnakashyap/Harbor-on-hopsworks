c86f39bc349a3b1290e62ce76f806ab24e9ae5f2
root <root@hopsworks0.logicalclocks.com>
2020-07-23 08:32:19 +0000

    end cases check for create project script with exceptions

--- integration-scripts/project_creation.py
+++ integration-scripts/project_creation.py
@@ -1,52 +1,38 @@
 #!/usr/bin/python
 
 try:
-    import docker
-    import request
+    import requests
     import sys
 except ImportError:
     import pip
     pip.main(['install', 'docker'])
-    import docker
     import requests
     import sys
 
-
 def create_project(project_name):
-        create_project = {
+        try: 
+		create_project = {
   "count_limit": -1,
   "project_name": project_name,
-  "cve_whitelist": {
-    "items": [
-      {
-        "cve_id": ""
-      }
-    ],
-    "project_id": 0,
-    "id": 0,
-    "expires_at": 0
-  },
-  "storage_limit": -1,
-  "metadata": {
-    "enable_content_trust": "false",
-    "auto_scan": "false",
-    "severity": "none",
-    "reuse_sys_cve_whitelist": "false",
-    "public": "false",
-    "prevent_vul": "true"
-  }
+  "storage_limit": -1
+  
 }
 
-        resp = requests.post(r'https://{}/api/v2.0/projects'.format(harbor_host), json=create_project,verify=r'/etc/docker/certs.d/{}/ca.crt'.format(harbor_host),auth=(user,password),headers={'Content-Type':'application/json'})
-        if resp.status_code != 201:
-                print('Error creating project ! POST error with status code /api/projects {}'.format(resp.status_code))
-                return
+        	resp = requests.post(r'https://{}/api/v2.0/projects'.format(harbor_host), json=create_project,verify=r'/etc/docker/certs.d/{}/ca.crt'.format(harbor_host),auth=(user,password),headers={'Content-Type':'application/json'})
+        	if resp.status_code != 201:
+                	print('Error creating project ! POST error with status code /api/projects {}'.format(resp.status_code))
+                	return
         #print(r'{} project successfully created'.format(project_name))
-
+	except IOError:
+		print('Please verify the name of the Harbor registry or check if the certificate required to access the Harbor Registry is located in path: "/etc/docker/certs.d/<registryip>/"')
+		
 if __name__ == '__main__':
 
         #Inputs to be obtained from Hopsworks
-        harbor_host= str(sys.argv[1])
+        if len(sys.argv)<5:
+                print("Fatal: Please include the required arguments in the command line.")
+                sys.exit(1)
+	harbor_host= str(sys.argv[1])
         user= str(sys.argv[2])
         password= str(sys.argv[3])
         project_name = str(sys.argv[4])
